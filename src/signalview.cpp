99686960e4e0b68e63aca63bc5755f7b2932891f
Malte Kießling <mkalte@mkalte.me>
2020-03-22 17:18:13 +0100

    more plot functionality

---
+++ src/signalview.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mkalte on 22/03/2020.
+//
+
+#include "signalview.h"
---
+++ src/signalview.h
@@ -0,0 +1,11 @@
+//
+// Created by mkalte on 22/03/2020.
+//
+
+#ifndef LAA_SIGNALVIEW_H
+#define LAA_SIGNALVIEW_H
+
+class SignalView {
+};
+
+#endif //LAA_SIGNALVIEW_H
